0fc1885ded9fc4701fa30f83945b3e8eb3dd20fe
lemanh-tuong <letuong05092000@gmail.com>
2023-02-21 23:38:08 +0700

    update type getSearchParams

--- src/services/utils/getSearchParams.ts
+++ src/services/utils/getSearchParams.ts
@@ -1,7 +1,7 @@
 import { AnyObject, Searcher } from 'services/@types/SearchParams';
 
 export const getSearchParams = <T extends AnyObject>(searcher: Searcher<T>) => {
-  return Object.keys(searcher).reduce<Record<string, string>>((res, searchKey) => {
+  return Object.keys(searcher).reduce<Partial<Record<keyof T, string>>>((res, searchKey) => {
     const searchItem = searcher[searchKey];
     if (searchItem) {
       const { operator, value } = searchItem;
