ec481bf134447a3b4949de49954f25d2db74cd4c
lemanh-tuong <letuong05092000@gmail.com>
2023-03-09 08:29:42 +0700

    update type definition cho search params -> fix lỗi nhận cả key function

--- src/services/@types/SearchParams.ts
+++ src/services/@types/SearchParams.ts
@@ -1,17 +1,25 @@
+// Built-in type
 export type AnyObject = Record<string, any>;
 type AnyArray = any[];
+type AnyFunction = (...args: any[]) => any;
 type PrimitiveType = string | number | boolean | null | undefined | AnyObject | AnyArray;
 
-type PickKeysByValue<T, V> = { [K in keyof T]: T[K] extends V ? K : never }[keyof T];
-type PickProperties<T, P> = Pick<T, PickKeysByValue<T, P>>;
-type GetKeyWithTypes<T, P> = Exclude<keyof PickProperties<T, P>, undefined>;
+// Utils
+type PickKeysByValue<T, V> = {
+  [K in keyof T]: T[K] extends AnyFunction ? never : T[K] extends V ? K : never;
+}[keyof T];
 
+type PickProperties<T, P> = Pick<T, PickKeysByValue<T, P>>;
+
+type GetKeyWithTypes<T, P> = Exclude<keyof PickProperties<T, P>, never>;
+
+// Mains
 type SubKeys<T, K extends string> = K extends keyof T ? `${K}.${PathKeyOfObject<T[K]>}` : never;
 
 type PathKeyOfObject<T> = object extends T
   ? string
   : T extends any[]
-  ? Extract<GetKeyWithTypes<T[number], PrimitiveType>, string> | SubKeys<T, Extract<GetKeyWithTypes<T[number], PrimitiveType>, string>>
+  ? Extract<GetKeyWithTypes<T[number], PrimitiveType>, string> | SubKeys<T[number], Extract<GetKeyWithTypes<T[number], PrimitiveType>, string>>
   : T extends object
   ? Extract<GetKeyWithTypes<T, PrimitiveType>, string> | SubKeys<T, Extract<GetKeyWithTypes<T, PrimitiveType>, string>>
   : never;
