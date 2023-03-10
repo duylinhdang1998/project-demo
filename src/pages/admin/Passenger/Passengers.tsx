00f5270a50eb0bac090474b1920d92412a4cc03a
lemanh-tuong <letuong05092000@gmail.com>
2023-03-10 22:20:01 +0700

    lên khung UI + logic cho chức năng batch email

--- src/pages/admin/Passenger/Passengers.tsx
+++ src/pages/admin/Passenger/Passengers.tsx
@@ -1,16 +1,22 @@
-import { Box, Grid, useMediaQuery, useTheme } from '@mui/material';
+import ClearIcon from '@mui/icons-material/Clear';
+import DoneAllIcon from '@mui/icons-material/DoneAll';
+import { Box, Dialog, DialogTitle, Divider, Grid, IconButton, Stack, Typography, useMediaQuery, useTheme } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import Button from 'components/Button/Button';
 import FilterTicket from 'components/FilterTicket/FilterTicket';
+import FormVerticle from 'components/FormVerticle/FormVerticle';
 import HeaderLayout from 'components/HeaderLayout/HeaderLayout';
 import SendIcon from 'components/SvgIcon/SendIcon';
+import ToastCustom from 'components/ToastCustom/ToastCustom';
 import { useAppDispatch } from 'hooks/useAppDispatch';
 import { isEmpty } from 'lodash';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
+import { toast } from 'react-toastify';
 import { Passenger } from 'services/models/Passenger';
 import { passengersActions } from 'store/passengers/passengersSlice';
+import { EmailEditor } from './components/EmailEditor';
 import TablePassenger from './components/TablePassenger';
 import { fieldsSearch } from './constants';
 
@@ -21,10 +27,15 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-interface Values {
+interface SearchFormValues {
   email: string;
   name: string;
   phone: string;
+}
+
+export interface BatchMailFormValues {
+  subject: string;
+  description: string;
 }
 
 export default function Passengers() {
@@ -33,23 +44,43 @@ export default function Passengers() {
   const classes = useStyles();
   const matches = useMediaQuery('(min-width:1200px)');
 
-  const [disabled, setDisabled] = useState(true);
+  const [selectedPassengers, setSelectedPassengers] = useState<Passenger[]>([]);
+  const [openEmailEditor, setOpenEmailEditor] = useState(false);
+  const [isSending, setIsSending] = useState(false);
 
   const dispatch = useAppDispatch();
 
-  const { control, handleSubmit } = useForm<Values>({
-    defaultValues: {
-      email: '',
-      name: '',
-      phone: '',
-    },
-  });
+  const searchForm = useForm<SearchFormValues>();
+  const emailEditorForm = useForm<BatchMailFormValues>();
 
-  const handleBatch = () => {
-    console.log('batch_email');
+  const messages = useMemo(() => {
+    return ['subject', 'description'].reduce<Record<string, string>>((res, key) => {
+      return {
+        ...res,
+        [key]: t('translation:error_required', { name: key }),
+      };
+    }, {});
+  }, [t]);
+
+  const handleOpenEmailEditor = () => {
+    setOpenEmailEditor(true);
   };
 
-  const onSubmit = (values: Values) => {
+  const handleBatchEmail = async (values: BatchMailFormValues) => {
+    setIsSending(true);
+    try {
+      console.log(values);
+      toast(<ToastCustom type="success" text={t('passenger:send_email_success')} />, { className: 'toast-success' });
+      setOpenEmailEditor(false);
+      emailEditorForm.reset({ description: '', subject: '' });
+    } catch {
+      toast(<ToastCustom type="error" text={t('passenger:send_email_error')} />, { className: 'toast-error' });
+    } finally {
+      setIsSending(false);
+    }
+  };
+
+  const onSubmit = (values: SearchFormValues) => {
     dispatch(
       passengersActions.getPassengersRequest({
         page: 0,
@@ -63,10 +94,6 @@ export default function Passengers() {
     );
   };
 
-  const handleSelect = (selectedRows: Passenger[]) => {
-    setDisabled(isEmpty(selectedRows));
-  };
-
   useEffect(() => {
     dispatch(
       passengersActions.getPassengersRequest({
@@ -78,6 +105,86 @@ export default function Passengers() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const renderEmailEditor = () => {
+    if (!openEmailEditor || isEmpty(selectedPassengers)) {
+      return null;
+    }
+    return (
+      <Dialog open onClose={() => setOpenEmailEditor(false)}>
+        <Box minWidth={500} padding="24px">
+          <Stack direction="row" justifyContent="space-between" alignItems="center">
+            <DialogTitle sx={{ padding: '0 !important' }}>{t('translation:batch_email')}</DialogTitle>
+            <IconButton onClick={() => setOpenEmailEditor(false)}>
+              <ClearIcon />
+            </IconButton>
+          </Stack>
+          <Divider variant="middle" sx={{ margin: '16px 0' }} />
+          <FormVerticle
+            errors={emailEditorForm.formState.errors}
+            messages={messages}
+            fields={[{ id: 'subject', label: 'subject', type: 'text', required: true }]}
+            control={emailEditorForm.control}
+            filterKey="passenger"
+          />
+          <EmailEditor
+            label="description"
+            control={emailEditorForm.control}
+            errors={emailEditorForm.formState.errors}
+            description={emailEditorForm.getValues().description}
+            messages={messages}
+            required
+            onChange={value => {
+              emailEditorForm.setValue('description', value);
+            }}
+          />
+          <Stack sx={{ marginTop: '24px' }} direction="row" justifyContent="space-between" alignItems="center">
+            <Stack direction="row" alignItems="center">
+              <DoneAllIcon sx={{ color: 'rgba(133, 140, 147, 1)', marginRight: '6px' }} />
+              <Typography
+                sx={{
+                  color: 'rgba(133, 140, 147, 1)',
+                  fontSize: '13px',
+                  '::first-letter': {
+                    color: 'rgba(69, 72, 94, 1)',
+                    fontSize: '14px',
+                  },
+                }}
+              >
+                {t('passenger:total_users_selected', { total: selectedPassengers.length })}
+              </Typography>
+            </Stack>
+            <Stack direction="row" alignItems="center">
+              <Button
+                loading={isSending}
+                sx={{
+                  color: 'rgba(26, 166, 238, 1)',
+                  backgroundColor: 'rgba(255, 255, 255, 1)',
+                  border: '1px solid rgba(26, 166, 238, 1)',
+                  padding: '10px 38px',
+                  marginRight: '16px',
+                }}
+              >
+                {t('translation:cancel')}
+              </Button>
+              <Button
+                sx={{
+                  backgroundColor: 'rgba(26, 166, 238, 1)',
+                  color: 'rgba(255, 255, 255, 1)',
+                  border: '1px solid rgba(26, 166, 238, 1)',
+                  padding: '10px 38px',
+                }}
+                backgroundButton="rgba(26, 166, 238, 1)"
+                onClick={emailEditorForm.handleSubmit(handleBatchEmail)}
+              >
+                {t('translation:send')}
+              </Button>
+            </Stack>
+          </Stack>
+        </Box>
+      </Dialog>
+    );
+  };
+
   return (
     <Box>
       <HeaderLayout activeSideBarHeader={t('passenger:passengers')} />
@@ -86,14 +193,14 @@ export default function Passengers() {
           <Grid item xs={12} md={8}>
             <Grid container columns={8} rowSpacing={2}>
               <Grid item xs={8} md={7}>
-                <FilterTicket control={control} fields={fieldsSearch} filterKey="passenger" numberColumns={matches ? 3 : 10} />
+                <FilterTicket control={searchForm.control} fields={fieldsSearch} filterKey="passenger" numberColumns={matches ? 3 : 10} />
               </Grid>
               <Grid item xs={8} md={1} sx={{ alignSelf: 'flex-end', marginLeft: matches ? '-48px' : '0px' }}>
                 <Button
                   variant="contained"
                   sx={{ width: matches ? '170px' : '100%' }}
                   backgroundButton={theme.palette.primary.main}
-                  onClick={handleSubmit(onSubmit)}
+                  onClick={searchForm.handleSubmit(onSubmit)}
                 >
                   {t('translation:search')}
                 </Button>
@@ -104,17 +211,18 @@ export default function Passengers() {
             <Button
               backgroundButton="#33CC7F"
               sx={{ alignSelf: 'flex-end' }}
-              disabled={disabled}
+              disabled={isEmpty(selectedPassengers)}
               startIcon={<SendIcon />}
               className={classes.btnAdd}
-              onClick={handleBatch}
+              onClick={handleOpenEmailEditor}
             >
               {t('translation:batch_email')}
             </Button>
           </Grid>
         </Grid>
-        <TablePassenger onSelect={handleSelect} />
+        <TablePassenger onSelect={setSelectedPassengers} />
       </Box>
+      {renderEmailEditor()}
     </Box>
   );
 }
