31398e2119f2e0b8919ca646591ca0cad70cd57b
lemanh-tuong <letuong05092000@gmail.com>
2023-03-22 18:26:44 +0700

    lắp webpack-bundle-analyzer

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,14 @@
+const rewireWebpackBundleAnalyzer = require('react-app-rewire-webpack-bundle-analyzer')
+
 module.exports = function override(config, env) {
-  //do stuff with the webpack config...
-  return config;
-};
+  // ...
+
+  if (env === 'production') {
+    config = rewireWebpackBundleAnalyzer(config, env, {
+      analyzerMode: 'static',
+      reportFilename: 'report.html'
+    })
+  }
+
+  return config
+}
