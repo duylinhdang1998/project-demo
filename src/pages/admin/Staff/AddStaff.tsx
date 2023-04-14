1e8d0092442c00dfeb8e82309bce392ec09ceb4d
lemanh-tuong <letuong05092000@gmail.com>
2023-04-14 23:27:41 +0700

    bổ sung message lỗi

--- src/pages/admin/Staff/AddStaff.tsx
+++ src/pages/admin/Staff/AddStaff.tsx
@@ -54,7 +54,7 @@ export default function AddStaff() {
       >
         <Box width="100%" display="flex" justifyContent="center">
           <Box bgcolor="#fff" borderRadius="4px" width={{ xs: '100%', md: '80%' }} padding="24px">
-            <StepForm isEditAction={isEditAction} startStep={location.state?.isConsultSchedule ? 2 : 1} />
+            <StepForm isEditAction={isEditAction} startStep={location.state?.isConsultSchedule ? 2 : 0} />
           </Box>
         </Box>
       </LayoutDetail>
