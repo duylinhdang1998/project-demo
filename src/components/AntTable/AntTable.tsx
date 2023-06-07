37f9e78ce5f5d34a8fe6e5eadb474d23af078fdb
lemanh-tuong <letuong05092000@gmail.com>
2023-06-07 20:00:36 +0700

    fix:TBUS-22 -> Bỏ thanh scroll ở tất cả màn Listing

--- src/components/AntTable/AntTable.tsx
+++ src/components/AntTable/AntTable.tsx
@@ -1,14 +1,12 @@
-import { useMediaQuery } from '@mui/material';
 import { Table } from 'antd';
 import { TableProps } from 'antd/lib/table';
 import 'antd/lib/table/style/css';
 import './styles.scss';
 
 export default function AntTable<T extends object = any>({ ...props }: TableProps<T>) {
-  const matches = useMediaQuery('(min-width:1366px)');
   return (
     <div className="tbus-table">
-      <Table {...props} scroll={{ y: '100vh', x: matches ? undefined : 1300 }} />
+      <Table {...props} />
     </div>
   );
 }
