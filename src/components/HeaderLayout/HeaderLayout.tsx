aca708cce45f51f6111d4365acbf39ac314836a0
lemanh-tuong <letuong05092000@gmail.com>
2023-06-07 22:02:51 +0700

    fix:TBUS-1

--- src/components/HeaderLayout/HeaderLayout.tsx
+++ src/components/HeaderLayout/HeaderLayout.tsx
@@ -4,6 +4,7 @@ import { makeStyles } from '@mui/styles';
 import { Box } from '@mui/system';
 import AccountDropdown from 'components/AccountDropdown/AccountDropdown';
 import ChangeLanguage from 'components/ChangeLanguage/ChangeLanguage';
+import env from 'env';
 import { useAppDispatch } from 'hooks/useAppDispatch';
 import { getTotalRemainingDays } from 'pages/admin/Subscription/utils/getTotalRemainingDays';
 import { getTotalTrialDays } from 'pages/admin/Subscription/utils/getTotalTrialDays';
@@ -14,7 +15,10 @@ import { useNavigate } from 'react-router-dom';
 import { selectProfile } from 'store/profile/selectors';
 import { selectSubscriptions } from 'store/subscriptions/selectors';
 import { subscriptionsActions } from 'store/subscriptions/subscriptionsSlice';
+import { getDomainName } from 'utils/getDomainName';
 import { getUrlOfResource } from 'utils/getUrlOfResource';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
+import { isEmpty } from 'lodash-es';
 
 interface HeaderLayoutProps {
   onToggleDrawer?: () => void;
@@ -54,7 +58,7 @@ function HeaderLayout({ activeSideBarHeader, subTitleHeader }: HeaderLayoutProps
   const classes = useStyles();
 
   const { profile } = useSelector(selectProfile);
-  const { currentSubscription } = useSelector(selectSubscriptions);
+  const { currentSubscription, subscriptions } = useSelector(selectSubscriptions);
   const dispatch = useAppDispatch();
 
   const navigate = useNavigate();
@@ -78,6 +82,9 @@ function HeaderLayout({ activeSideBarHeader, subTitleHeader }: HeaderLayoutProps
   };
 
   useEffect(() => {
+    if (isEmpty(subscriptions)) {
+      dispatch(subscriptionsActions.getSubscriptionsRequest({}));
+    }
     if (!currentSubscription) {
       dispatch(subscriptionsActions.getCurrentSubscriptionRequest({}));
     }
@@ -85,11 +92,17 @@ function HeaderLayout({ activeSideBarHeader, subTitleHeader }: HeaderLayoutProps
   }, []);
 
   const renderUpgradeSubscriptionMessage = () => {
-    if (currentSubscription?.subscriptionType === 'TRIAL') {
+    if (totalRemainingTrialDays <= 7) {
       return (
         <Box className={classes.subcribe}>
           <Typography component="span" fontSize={{ mobile: 12, tablet: 14 }} fontWeight="400">
-            {t('account:trial_warning_message', { totalTrialDays, totalRemainingTrialDays })}{' '}
+            {t('account:subscription_expire_warning_message', {
+              totalTrialDays,
+              totalRemainingTrialDays,
+              subscriptionName: subscriptions
+                .find(subscription => subscription.subscriptionType === currentSubscription?.subscriptionType)
+                ?.name?.toLowerCase(),
+            })}{' '}
             <Typography
               onClick={() => navigate('/account/subscription-package')}
               sx={{ cursor: 'pointer' }}
@@ -97,7 +110,11 @@ function HeaderLayout({ activeSideBarHeader, subTitleHeader }: HeaderLayoutProps
               variant="subtitle1"
               fontSize={{ mobile: 12, tablet: 14 }}
             >
-              {t('account:upgrade_subscription_message')}
+              {t('account:upgrade_subscription_message', {
+                subscriptionName: subscriptions
+                  .find(subscription => subscription.subscriptionType === currentSubscription?.subscriptionType)
+                  ?.name?.toLowerCase(),
+              })}
             </Typography>
           </Typography>
         </Box>
@@ -156,6 +173,22 @@ function HeaderLayout({ activeSideBarHeader, subTitleHeader }: HeaderLayoutProps
             </Typography>
           </Box>
           <Stack direction="row" spacing={{ mobile: 2, desktop: 4 }} alignItems="center">
+            <div className={classes.link}>
+              <LanguageOutlinedIcon sx={{ fontSize: 40, color: '#1AA6EE', marginRight: '10px' }} />
+              <Typography
+                component="span"
+                fontSize="14px"
+                display={{
+                  mobile: 'none',
+                  tablet: 'block',
+                }}
+              >
+                {t('your_website')}:
+              </Typography>
+              <a href={`https://${getDomainName()}.${env.baseWebDomain}`} target="_blank" className={classes.weblink}>
+                {getDomainName()}.{env.baseWebDomain}
+              </a>
+            </div>
             <ChangeLanguage />
             <AccountDropdown avatar={getUrlOfResource(profile.profileImage)} name={profile.name} email={profile.email} />
           </Stack>
