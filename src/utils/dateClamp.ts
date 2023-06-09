55bfdfd933a63495b0cc001d5755687f4ddee54b
lemanh-tuong <letuong05092000@gmail.com>
2023-06-09 22:50:05 +0700

    fix:TBUS-54 Không thể tick chọn off/ bỏ chọn off ngày hiện tại của step 3

--- src/utils/dateClamp.ts
+++ src/utils/dateClamp.ts
@@ -1,3 +1,12 @@
 export const dateClamp = (timestamp: number, start: number, end: number) => {
-  return timestamp > start && timestamp < end;
+  const startDate = new Date(start);
+  const endDate = new Date(end);
+  const checkDate = new Date(timestamp);
+
+  // Remove time components from dates
+  startDate.setHours(0, 0, 0, 0);
+  endDate.setHours(0, 0, 0, 0);
+  checkDate.setHours(0, 0, 0, 0);
+
+  return checkDate >= startDate && checkDate <= endDate;
 };
