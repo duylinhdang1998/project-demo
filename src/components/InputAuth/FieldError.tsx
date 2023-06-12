7104096b8fea955659692521e3af3c3bccceae01
lemanh-tuong <letuong05092000@gmail.com>
2023-06-12 21:30:01 +0700

    refix:TBUS-68 - rút gọn message lỗi

--- src/components/InputAuth/FieldError.tsx
+++ src/components/InputAuth/FieldError.tsx
@@ -9,39 +9,15 @@ interface Props {
 }
 
 export const PASSWORD_VALID_PATTERN_MESSAGE = 'PASSWORD_VALID_PATTERN_MESSAGE';
-export const FieldError: FC<Props> = ({ fieldMessage, fieldValue }) => {
+export const FieldError: FC<Props> = ({ fieldMessage }) => {
   const classes = useStyles();
   const { t } = useTranslation(['translation']);
 
   if (fieldMessage === PASSWORD_VALID_PATTERN_MESSAGE) {
     return (
-      <>
-        {typeof fieldValue === 'string' && fieldValue.length < 8 && (
-          <Typography component="p" className={classes.error} fontSize={12}>
-            - {t('translation:password_must_at_lease_8_character')}
-          </Typography>
-        )}
-        {typeof fieldValue === 'string' && fieldValue.search(/[0-9]/) < 0 && (
-          <Typography component="p" className={classes.error} fontSize={12}>
-            - {t('translation:password_must_at_lease_one_number_character')}
-          </Typography>
-        )}
-        {typeof fieldValue === 'string' && fieldValue.search(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/) < 0 && (
-          <Typography component="p" className={classes.error} fontSize={12}>
-            - {t('translation:password_must_at_lease_one_special_character')}
-          </Typography>
-        )}
-        {typeof fieldValue === 'string' && fieldValue.search(/[a-z]/) < 0 && (
-          <Typography component="p" className={classes.error} fontSize={12}>
-            - {t('translation:password_must_at_lease_one_lowercase_letter')}
-          </Typography>
-        )}
-        {typeof fieldValue === 'string' && fieldValue.search(/[A-Z]/) < 0 && (
-          <Typography component="p" className={classes.error} fontSize={12}>
-            - {t('translation:password_must_at_lease_one_uppercase_letter')}
-          </Typography>
-        )}
-      </>
+      <Typography component="p" className={classes.error} fontSize={12}>
+        {t('translation:password_is_not_strong')}
+      </Typography>
     );
   }
   return (
