2ce4bdaa808a04d83323c412e8619ec36fb81ea6
lemanh-tuong <letuong05092000@gmail.com>
2023-06-29 11:09:07 +0700

    update getNameOfResource

--- src/utils/getNameOfResource.ts
+++ src/utils/getNameOfResource.ts
@@ -1,12 +1,15 @@
 import { ImageResource, PDFResource } from 'services/models/Resource';
 
 export const getNameOfResource = (resource: ImageResource | PDFResource | undefined | null) => {
-  if (!resource) {
+  if (!resource || typeof resource !== 'object') {
     return '';
   }
   if ('filename' in resource) {
     return resource.filename;
   }
   const splited = resource.publicUrl?.split('/');
-  return splited[splited.length - 1];
+  if (splited) {
+    return splited[splited.length - 1];
+  }
+  return '';
 };
