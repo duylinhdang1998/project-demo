64ea7a046b00627527c9b284b1367e2b9e455997
steventran <trantuyen25598@gmail.com>
2023-07-19 09:35:07 +0700

    fix sub route list

--- src/pages/admin/Routers/utils/getRoutePointsWithRouteType.ts
+++ src/pages/admin/Routers/utils/getRoutePointsWithRouteType.ts
@@ -1,7 +1,11 @@
 import { Route } from 'services/models/Route';
 
 export const getMainRoutePoints = (routePoints: Route['routePoints']) => {
-  return routePoints.filter(routePoint => routePoint.routeType === 'MAIN_ROUTE');
+  /**
+   * Đoạn này phải sort theo durationTime do đi tới điểm sau sẽ lâu hơn điểm trước
+   * Như này dữ liệu mới đúng
+   */
+  return routePoints.filter(routePoint => routePoint.routeType === 'MAIN_ROUTE').sort((a, b) => a.durationTime - b.durationTime);
 };
 
 export const getSubRoutePoints = (routePoints: Route['routePoints']) => {
