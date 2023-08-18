1fa0d15dde921e115837dbda755800fd444dff3d
ductrinh <trinhcongduc1998@gmail.com>
2023-08-18 21:44:39 +0700

    fix bugs

--- src/env.ts
+++ src/env.ts
@@ -13,7 +13,7 @@ const env: ENV = {
   apiEndPoint: import.meta.env.VITE_APP_API_END_POINT || '',
   theme: import.meta.env.VITE_APP_THEME || 'light',
   appLanguage: import.meta.env.VITE_APP_LANGUAGE || 'en',
-  isDevMode: import.meta.env.DEV || import.meta.env.VITE_APP_STAGE === 'local' || import.meta.env.VITE_APP_STAGE === 'development',
+  isDevMode: import.meta.env.DEV || import.meta.env.VITE_APP_STAGE === 'local',
   tinyMCEApiKey: import.meta.env.VITE_APP_TINY_MCE_API_KEY,
   baseCmsDomain: import.meta.env.VITE_APP_BASE_CMS_DOMAIN,
   baseWebDomain: import.meta.env.VITE_APP_BASE_WEB_DOMAIN,
